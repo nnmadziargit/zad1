3d4dd75c0b06a619ae59fc8139b1eeeca5e33045
nnmadziar <nnmadziar@icloud.com>
2024-11-14 12:02:30 +0100

    wykonianie

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,24 +1,55 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace TextFormatter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            UpdateProgress();
+        }
+
+
+        private void UpdateTextFormatting(object sender, RoutedEventArgs e)
+        {
+            FontWeight fontWeight = BoldCheckBox.IsChecked == true ? FontWeights.Bold : FontWeights.Normal;
+            FontStyle fontStyle = ItalicCheckBox.IsChecked == true ? FontStyles.Italic : FontStyles.Normal;
+            TextDecorationCollection textDecorations = UnderlineCheckBox.IsChecked == true ? TextDecorations.Underline : null;
+
+            MainTextBox.FontWeight = fontWeight;
+            MainTextBox.FontStyle = fontStyle;
+            MainTextBox.TextDecorations = textDecorations;
+
+       
+            MainTextBox.FontSize = FontSizeSlider.Value;
+
+            
+            string selectedColor = ((ComboBoxItem)FontColorComboBox.SelectedItem)?.Content.ToString();
+            MainTextBox.Foreground = selectedColor switch
+            {
+                "Red" => Brushes.Red,
+                "Green" => Brushes.Green,
+                "Blue" => Brushes.Blue,
+                _ => Brushes.Black,
+            };
+            UpdateProgress();
+        }
+
+
+        private void UpdateProgress()
+        {
+            int progress = 0;
+            if (BoldCheckBox.IsChecked == true) progress++;
+            if (ItalicCheckBox.IsChecked == true) progress++;
+            if (UnderlineCheckBox.IsChecked == true) progress++;
+            if (FontSizeSlider.Value != 12) progress++;
+            if (FontColorComboBox.SelectedIndex > -1) progress++;
+
+            ProgressBar.Value = progress;
         }
     }
 }
+
